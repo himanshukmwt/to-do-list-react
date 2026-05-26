b5d8f0951807a65690106229d36e9cc652b1da4c
himanshukmwt <himanshukumawat8824@gmail.com>
2026-05-26 17:00:11 +0530

    moveToUp and moveToDown function added

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -18,6 +18,22 @@ function ToDoList() {
     const updatedTask = tasks.filter((_, i) => i !== index);
     setTasks(updatedTask);
   }
+
+  function moveToUp(index){
+    if(index>0){
+        const updatedTask=[...tasks];
+        [updatedTask[index],updatedTask[index-1]]= [updatedTask[index-1],updatedTask[index]];
+        setTasks(updatedTask);
+    }
+  }
+
+   function moveToDown(index){
+    if(index<tasks.length-1){
+        const updatedTask=[...tasks];
+        [updatedTask[index],updatedTask[index+1]]= [updatedTask[index+1],updatedTask[index]];
+        setTasks(updatedTask);
+    }
+  }
   return (
     <div className="to-do-list">
       <h1>To-Do List</h1>
@@ -45,6 +61,12 @@ function ToDoList() {
             <span className="text">{task}</span>
             <button className="delete-btn" onClick={() => deleteTask(index)}>
               Delete
+            </button>
+            <button className="move-btn" onClick={() => moveToUp(index)}>
+              &#8593;
+            </button>
+            <button className="move-btn" onClick={() => moveToDown(index)}>
+              &#8595;
             </button>
           </li>
         ))}
